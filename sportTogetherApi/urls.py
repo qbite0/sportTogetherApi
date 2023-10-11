2a092ec5555f107db4c3b234d7f96390e78fafd0
Max <qbite0@gmail.com>
2023-10-11 17:42:45 +0700

    password error

--- sportTogetherApi/urls.py
+++ sportTogetherApi/urls.py
@@ -1,13 +1,17 @@
 from django.urls import path, include
 from django.contrib import admin
 from django.contrib.auth.models import User, Group
-from rest_framework import routers, serializers, viewsets
+from rest_framework import routers, serializers, viewsets, permissions
+from rest_framework.decorators import action
 
 # Serializers define the API representation.
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ['url', 'username', 'email', 'groups']
+        fields = ['url', 'username', 'email', 'groups', 'password']
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
 
 class GroupSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
@@ -16,12 +20,25 @@ class GroupSerializer(serializers.HyperlinkedModelSerializer):
 
 # ViewSets define the view behavior.
 class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]        
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
+router.register(r'groups', GroupViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
